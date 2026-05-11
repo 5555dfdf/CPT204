73eb1064e185f5a96147055ed7fcd94728a96fcf
5555dfdf <3123048821@qq.com>
2026-05-11 14:05:49 +0800

    5.11 实现bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,11 +3,41 @@ import java.util.List;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        List<Location> locationList = CsvReader.reader("dataset\\candidates_A.csv");
+        String[] files = {
+                "dataset\\candidates_A.csv",
+                "dataset\\candidates_B.csv",
+                "dataset\\candidates_C.csv"
+        };
+        for (String filePath : files){
+            List<Location> locationList = CsvReader.reader(filePath);
+            long startTime = System.nanoTime();
+            bubbleSort(locationList,  new LocationComparator());
+            long endTime = System.nanoTime();
+            long durationNano = endTime - startTime;
+            double durationMs = (double) durationNano / 1000000;
+            System.out.printf("Bubble sort for %s took: %.3f ms%n", filePath.substring(8), durationMs);
+            for (int i = 0; i < 10; i++){
+                System.out.println(locationList.get(i).toString());
+            }
+            System.out.println("================================================");
+        }
     }
 
     public static <T> void bubbleSort(List<T> list, Comparator<T> comparator){
-
+        for (int i = 0; i < list.size()-1; i++){
+            boolean swap = false;
+            for (int j = 0; j< list.size()-i-1; j ++){
+                if (comparator.compare(list.get(j), list.get(j+1) )> 0){
+                    T temp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, temp);
+                    swap = true;
+                }
+            }
+            if (!swap){
+                break;
+            }
+        }
 
     }
 }
