dab0eecf6364f7a5aba672fad2da70d4cce96785
5555dfdf <3123048821@qq.com>
2026-05-11 14:05:34 +0800

    5.11 实现comparator

--- src/LocationComparator.java
+++ src/LocationComparator.java
@@ -3,7 +3,17 @@ import java.util.Comparator;
 public class LocationComparator implements Comparator<Location> {
 
     @Override
-    public int compare(Location a, Location b){
-        return 2;
+    public int compare(Location a, Location b) {
+        // 1. 优先级
+        if (a.getPriority() > b.getPriority()) {
+            return -1;  // a 优先级更高 → 排在前面
+        } else if (a.getPriority() < b.getPriority()) {
+            return 1;   // a 优先级更低 → 排在后面
+        }
+
+        // 2. 地点编号 升序排列
+        int numA = Integer.parseInt(a.getLocationName().substring(1));
+        int numB = Integer.parseInt(b.getLocationName().substring(1));
+        return Integer.compare(numA, numB);
     }
 }
